0b656ec3506a547de1068d0624ea35794df8a284
HelpfulTeammate <133812939+HelpfulTeammate@users.noreply.github.com>
2023-08-18 10:25:15 +0800

    Update intl_phone_field.dart

--- lib/intl_phone_field.dart
+++ lib/intl_phone_field.dart
@@ -328,23 +328,21 @@ class _IntlPhoneFieldState extends State<IntlPhoneField> {
       }
     }
 
-final initialPhoneNumber = PhoneNumber(
-  countryISOCode: _selectedCountry.code,
-  countryCode: '+${_selectedCountry.dialCode}',
-  number: widget.initialValue ?? '',
-);
+    final initialPhoneNumber = PhoneNumber(
+      countryISOCode: _selectedCountry.code,
+      countryCode: '+${_selectedCountry.dialCode}',
+      number: widget.initialValue ?? '',
+    );
 
-if (widget.validator != null) {
-  final value = widget.validator!.call(initialPhoneNumber);
+    final value = widget.validator?.call(initialPhoneNumber);
 
-  if (value is String) {
-    validatorMessage = value;
-  } else if (value is Future) {
-    value.then((msg) {
-      validatorMessage = msg;
-    });
-  }
-}
+    if (value is String) {
+      validatorMessage = value;
+    } else {
+      (value as Future).then((msg) {
+        validatorMessage = msg;
+      });
+    }
   }
 
   Future<void> _changeCountry() async {
